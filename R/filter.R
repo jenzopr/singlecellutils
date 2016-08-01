97f382713ef61430e989ac70cfe9e44fde352372
Jens Preussner <jens.preussner@mpi-bn.mpg.de>
2016-08-01 11:25:51 +0200

    Adding the aggregate filter function for genefilter.

---
+++ R/filter.R
@@ -0,0 +1,15 @@
+#' EpOverA returns a filter function with bindings for A. This function evaluates to TRUE if the aggregate of the arguments elements are larger than A.
+#'
+#' @param A The value you want to exceed.
+#' @param na.rm Whether NAs should be removed.
+#'
+#' @return A function with bindings for A.
+#'
+#' @export
+EpOverA <- function(A = 4, na.rm = TRUE) {
+  function(x) {
+    if(na.rm)
+      x <- x[!is.na(x)]
+    log2(sum(x)+1) > A
+  }
+}
