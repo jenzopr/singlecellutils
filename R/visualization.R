83506459f3fe87347bbd6da03b739f2cb204ded5
Jens Preussner <jens.preussner@mpi-bn.mpg.de>
2017-02-13 15:12:56 +0100

    Use any color palette instead of RColorBrewer

--- R/visualization.R
+++ R/visualization.R
@@ -38,13 +38,13 @@ vistSNE <- function(tsne, k = NULL, use.pal = "Dark2", add.center = T, medoids =
 #' @param tsne The data matrix from a tSNE dimension reduction call
 #' @param data A numerical vector on which the coloring is based
 #' @param data.name The name of the data, shown in plot.main
-#' @param use.pal The RColorBrewer color palette
+#' @param palette The color palette
 #' @param ... Other parameters passed to plot
 #'
 #' @return NULL
 #'
 #' @export
-colortSNE <- function(tsne, data, data.name = NULL, use.pal = "RdBu", ...) {
+colortSNE <- function(tsne, data, data.name = NULL, palette = RColorBrewer::brewer.pal(10, "Dark2"), ...) {
   if (length(data) != nrow(tsne)) {
     stop("Data is not same length as tsne.")
   }
@@ -53,8 +53,7 @@ colortSNE <- function(tsne, data, data.name = NULL, use.pal = "RdBu", ...) {
   } else {
     main <- ""
   }
-  pal <- colorRampPalette(RColorBrewer::brewer.pal(10, use.pal))
-  color <- rev(pal(100))[as.numeric(cut(data, breaks = 100))]
+  color <- palette[as.numeric(cut(data, breaks = length(palette)))]
   plot(tsne[, 1], tsne[, 2], xlab = "tSNE dimension 1", ylab = "tSNE dimension 2", main = main, col = color, ...)
   return(invisible(NULL))
 }
