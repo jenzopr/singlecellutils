3fea357278cf0c7c19a014325c890c57aeed4fbd
Jens Preussner <jens.preussner@mpi-bn.mpg.de>
2017-11-14 13:22:51 +0100

    Added functionality to create a SingleCellExpression object.

---
+++ R/infrastructure.R
@@ -0,0 +1,50 @@
+#' Creates a proper SingleCellExperiment object from expression tables and metadata
+#'
+#' @param data.files A named list with file paths to tab-separated expression matrices
+#' @param metadata.files A list with file paths to tab-separated metadata matrices
+#' @param cell.identifier The column name of cell identifiers in metadata.files (e.g. Sample, Cell)
+#' @param annotate.genes Boolean, whether or not bioMart should be used to annotate genes
+#' @param biomart.dataset The dataset that is used to fetch annotations (e.g. mmusculus_gene_ensembl)
+#' @param biomart.filter The filter that is used to match annotations to genes (e.g. ensembl_gene_id, if row.names are Ensembl Gene IDs)
+#' @param biomart.fields A character vector of field names to fetch
+#'
+#' @return A SingleCellExperiment object.
+#'
+#' @export
+createSingleCellExperiment <- function(data.files, metadata.files = NULL, cell.identifier = "Sample", annotate.genes = F, biomart.dataset = "mmusculus_gene_ensembl", biomart.filter = "ensembl_gene_id", biomart.fields = c("ensembl_gene_id","external_gene_name","gene_biotype","description")) {
+  #
+  # Read expression data
+  #
+  expression <- lapply(data.files, function(p) read.table(file = p, sep = "\t", row.names = 1, header = T))
+
+  #
+  # Annotate gene names
+  #
+  if (annotate.genes) {
+    identifier <- rownames(expression[[1]])
+    bm <- biomaRt::getBM(biomart.fields, filters = biomart.filter, values = identifier, mart = biomaRt::useMart("ensembl", dataset = biomart.dataset))
+    m <- match(identifier, bm[, biomart.filter])
+    data <- data.frame(bm[na.omit(m), ])
+    rownames(data) <- data[, biomart.filter]
+    colnames(data) <- biomart.fields
+
+    rowData <- data[,c(2:ncol(data))]
+  } else {
+    rowData <- NULL
+  }
+
+  #
+  # Assemble metadata
+  #
+  if (!is.null(metadata.files)) {
+    metadata_ <- lapply(metadata.files, function(p) metadata <- read.table(file = p, sep = "\t", header = T, stringsAsFactors = T, na.strings = "NA"))
+    metadata <- Reduce(function(a, b) dplyr::left_join(a, b, by = cell.identifier), metadata_)
+
+    m <- match(colnames(expression[1]), metadata[, cell.identifier])
+    colData <- metadata[na.omit(m), ]
+  } else {
+    colData <- NULL
+  }
+
+  scater::SingleCellExperiment(assays = expression, colData = phenoData, rowData = featureData)
+}
