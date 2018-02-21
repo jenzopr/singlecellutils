a33cce5527f22e3cf8665f79e68be5f2645f738b
Jens Preussner <jens.preussner@mpi-bn.mpg.de>
2018-02-21 10:00:01 +0100

    Added gene filter function from Weinreb et. al.

--- R/filter.R
+++ R/filter.R
@@ -13,3 +13,21 @@ EpOverA <- function(A = 4, na.rm = TRUE) {
     sum(x) > A
   }
 }
+
+#' FanoOverA returns a filter function with bindings for A. This function evaluates to TRUE if the Fano factor of the argument is larger than A.
+#'
+#' @param A The value that should be exceeded.
+#' @param na.rm Whether NAs should be removed.
+#'
+#' @return A function with bindings for A.
+#'
+#' @details See Weinreb et.al., https://doi.org/10.1093/bioinformatics/btx792
+#'
+#' @export
+FanoOverA <- function(A = 2, na.rm = TRUE) {
+  function(x) {
+    if (na.rm)
+      x <- x[!is.na(x)]
+    var(x) / mean(x) > A
+  }
+}
