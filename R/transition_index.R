508df22856f68924e78e61c2cc5d80b4c2c45ef6
Jens Preussner <jens.preussner@mpi-bn.mpg.de>
2018-05-25 13:41:49 +0200

    Updated transition index functions for work with a SingleCellExperiment object.

--- R/transition_index.R
+++ R/transition_index.R
@@ -1,12 +1,33 @@
-#' Calculate IC values for a data matrix (genes-by-cells)
+#' Adds the cirtical transition index to the object.
 #'
-#' @param data Data matrix
-#' @param p.val The p-value cutoff for correlation values to use in calculations
+#' @param object A SingleCellExperiment object.
+#' @param column Determines the column name of the \code{colData} slot to store results to.
+#' @param ... Additional parameters passed to \code{computeIC}.
+#'
+#' @return A SingleCellExperiment object with modified \code{colData} slot.
+#'
+#' @export
+add_transition_index <- function(object, column, ...) {
+  ic <- compute_transition_index(object, ...)
+  object <- SingleCellExperiment::mutate(object, !!column := ic$ic)
+  return(object)
+}
+
+#' Calculate the critical transition index for cells in a SingleCellExperiment object.
+#' The critical transition index is described in detail in http://www.pnas.org/content/114/9/2271.long.
+#'
+#' @param object A SingleCellExperiment object.
+#' @param exprs_values String indicating which assay contains the data that should be used to calculate the IC.
+#' @param features A character vector (of feature names), a logical vector or numeric vector (of indices) specifying the features to use for calculation. The default of NULL will use all features.
+#' @param pval The p-value cutoff for correlation values to use in calculations
 #'
 #' @return A list with the IC value (ic), as well as the used correlation values (within (GxG) / between (CxC))
 #' @export
-computeIC <- function(data, p.val = 0.01) {
-  d <- data
+compute_transition_index <- function(object, exprs_values, features = NULL, pval = 0.01) {
+  d <- SummarizedExperiment::assay(object, exprs_values)
+  if(!is.null(features)) {
+    d <- SummarizedExperiment::assay(object, exprs_values)[features, ]
+  }
 
   r <- Hmisc::rcorr(t(d), type="pearson")
   use <- (r$P < p.val & !is.na(r$P) & upper.tri(r$r))
@@ -19,23 +40,32 @@ computeIC <- function(data, p.val = 0.01) {
   list(ic = mean(within) / mean(between), within = within, between = between)
 }
 
-#' Run non-parametric bootstrapping for IC values and groups
+#' Run non-parametric bootstrapping for critical transition index values and a grouping variable
 #'
-#' @param data The expression matrix (genes-by-cells)
-#' @param groups A vector giving group information (will be coerced to factor)
-#' @param n The subsample size for non-parametric bootstrapping
-#' @param R The bootstrapping size
+#' @param object A SingleCellExperiment object.
+#' @param exprs_values String indicating which assay contains the data that should be used to calculate IC.
+#' @param grouping A single character indicating the column of \code{colData} to use as grouping, or a factor of \code{length > 1}.
+#' @param features A character vector (of feature names), a logical vector or numeric vector (of indices) specifying the features to use for calculation. The default of NULL will use all features.
+#' @param n The subsample size for non-parametric bootstrapping.
+#' @param R The bootstrapping size.
 #'
-#' @return A matrix with all bootstrap samples per grouping level (R rows, length(levels(group)) columns)
+#' @return A matrix with all bootstrap samples per grouping level (\code{R} rows, \code{length(levels(group))} columns)
 #' @export
-boot.ic <- function(data, groups, n = 20, R = 999, ...) {
-  if (!is.factor(groups)) {
-    groups <- factor(groups)
+bootstrap_transition_index <- function(object, exprs_values, grouping, features = NULL, n = 20, R = 999, ...) {
+  if(length(grouping == 1)) {
+    grouping <- SummarizedExperiment::colData(object)[[grouping]]
   }
 
-  b.ic <- lapply(levels(groups), function(g) {
-    d <- data[, groups == g]
-    boot::boot(d, function(x, p.val) computeIC(x, p.val)$ic, sim="parametric", R=R, ran.gen = boot_subsample, mle=n, ...)
+  if (!is.factor(grouping)) {
+    grouping <- factor(grouping)
+  }
+
+  b.ic <- lapply(levels(grouping), function(g) {
+    d <- SummarizedExperiment::assay(object, exprs_values)[, grouping == g]
+    if(!is.null(features)) {
+      d <- SummarizedExperiment::assay(object, exprs_values)[features, grouping == g]
+    }
+    boot::boot(d, function(x, p.val) compute_transition_index(x, p.val)$ic, sim="parametric", R=R, ran.gen = boot_subsample, mle=n, ...)
   })
 
   ic_values <- sapply(b.ic, function(l) l$t)
