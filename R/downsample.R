56a7a5c757eedaaa758a1de9c736b2836e9f504f
Jens Preussner <jens.preussner@mpi-bn.mpg.de>
2018-05-25 14:16:00 +0200

    Updated downsampling to work with SingleCellExperiment objects.

--- R/downsample.R
+++ R/downsample.R
@@ -1,73 +1,40 @@
-#' Thinning of a count matrix
-#' @param y The count matrix
-#' @param current.lib.sized The current library sizes
-#' @param target.lib.sizes The target library sizes
+#' Thinning of a expression matrix from a SingleCellExperiment object.
 #'
-#' @return A list with two components:
-#'   counts: The thinned count matrix
-#'   lib.sizes: The new library sizes
+#' @param object A SingleCellExperiment object.
+#' @param exprs_values String indicating which assay contains the data that should be used to perform thinning.
+#' @param current_libsizes The current library sizes (can directly be calculated from \code{object}).
+#' @param target_libsizes The target library sizes th thin expression to.
+#' @param return_as String indicating the name of the assay which contains the thinned expression values.
+#'
+#' @return A SingleCellExperiment object with a new assay containing the thinned values.
+#'
 #' @export
-thin.counts = function(y, current.lib.sizes = colSums(y),
-                       target.lib.sizes = min(current.lib.sizes)) {
-  n = dim(y)[1];
-  ncols = dim(y)[2];
+thin_counts <- function(object, exprs_values, current_libsizes = scater::librarySizeFactors(object, exprs_values),
+                       target_libsizes = min(current_libsizes), return_as = "downsampled") {
+  y <- SummarizedExperiment::assay(object, exprs_values)
+  n <- dim(y)[1];
+  m <- dim(y)[2];
 
-  if (any (target.lib.sizes > current.lib.sizes)) {
-    stop("Error: taget.lib.sizes > current.lib.sizes!");
+  if (any (target_libsizes > current_libsizes)) {
+    stop("Error: Found a target library size that is larger than its current library size.");
   }
 
-  if (length(target.lib.sizes) == 1) {
-    target.lib.sizes = rep(target.lib.sizes, ncols);
+  if (length(target_libsizes) == 1) {
+    target.lib.sizes = rep(target.lib.sizes, m);
   }
 
-  y.thinned = y;
+  y_thinned = y;
 
-  for (k in 1:ncols) {
-    keep.p =  target.lib.sizes[k]/current.lib.sizes[k]; # proportion of reads to keep
+  for (k in 1:m) {
+    keep.p =  target_libsizes[k]/current_libsizes[k]; # proportion of reads to keep
     if (keep.p == 1) {
-      y.thinned[, k] <- y[,k]
+      y_thinned[, k] <- y[,k]
     }
     else {
-      y.thinned[,k] = rbinom(n, y[,k], keep.p);
+      y_thinned[,k] = rbinom(n, y[,k], keep.p);
     }
   }
-  list(counts =y.thinned, lib.sizes = target.lib.sizes);
-}
 
-#' Downsampling of a count matrix to a target size
-#'
-#' @param counts The count matrix
-#' @param totalReads The library size vector (in millions)
-#' @param mean The mean target library size (in millions)
-#' @param sd The standard deviation around the mean, used to create target library sizes
-#' @param lowerDetectionLimit Number of counts considered as lower detection limit
-#'
-#' @return A list with the following components
-#'   expressed:
-downsample <- function(counts, totalReads, mean = 1, sd = 0.1, lowerDetectionLimit = 10) {
-  target_size <- rnorm(ncol(counts), mean=mean, sd=sd)
-
-  fraction <- abs(target_size) / totalReads
-  fraction <- ifelse(fraction > 1, 1, fraction)
-
-  new_total_reads <- totalReads * fraction
-  target_libsizes <- colSums(counts)*fraction
-  #plot(colSums(counts), target_libsizes, xlab="Total counts of core set", ylab="Downsampled counts of core set", pch=20, main=paste("Downsampled to mean", mean, "and sd",sd,sep=" "))
-
-  ds_counts <- thin.counts(counts, current.lib.sizes = colSums(counts), target.lib.sizes = target_libsizes)
-  ds_counts$counts[is.na(ds_counts$counts)] <- 0
-
-  expressed <- ds_counts$counts > lowerDetectionLimit
-
-  cell.order <- order(new_total_reads, decreasing = F)
-
-  r <- rollapply(cell.order, width=20, function(ci) { mean(colSums(expressed[,ci]))}, fill="extend")
-
-  r <- r / length(selected_features)
-
-  list(expressed = r, fraction = fraction[cell.order], total = totalReads[cell.order], downsampled = new_total_reads[cell.order])
-  # plot(1:length(cell.order), r, xlab="Window starting at cell", ylab="Percentage of mean detected features", pch=20, main=paste("Downsampled to mean", mean, "and sd",sd,sep=" "))
-  #
-  # m <- rollapply(cell.order, width=20, function(ci) {mean(new_total_reads[ci])}, fill="extend")
-  # plot(m, r, xlab="Mean total sequenced reads", ylab="Percent of detected genes", pch=20, main=paste("Downsampled to mean", mean, "and sd",sd,sep=" "))
+  SummarizedExperiment::assay(object, return_as) <- y_thinned
+  return(object)
 }
