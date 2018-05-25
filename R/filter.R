df76f50a9251ceb82e5c2e3928a9f2c16bb4aec1
Jens Preussner <jens.preussner@mpi-bn.mpg.de>
2018-05-25 11:11:22 +0200

    Added verb filter_genes.

--- R/filter.R
+++ R/filter.R
@@ -1,3 +1,27 @@
+#' Conveinience function to perform gene filtering using \code{genefilter}.
+#'
+#' @param object A SingleCellExperiment object.
+#' @param flist A named list with gene filters. Names should be callable functions, list items should be named paramters.
+#' @param exprs_values String indicating which assay contains the data that should be used for filtering.
+#'
+#' @return A SingleCellExperiment object with features passing the filter.
+#'
+#' @export
+filter_genes <- function(object, flist, exprs_values = "counts") {
+  filter_functions <- lapply(names(flist), function(f) {
+    fn <- strsplit(f, "::")[[1]]
+    what <- if(length(fn)==1) {
+      get(fn[[1]], envir=parent.frame(), mode="function")
+    } else {
+      get(fn[[2]], envir=asNamespace(fn[[1]]), mode="function")
+    }
+    do.call(what = what, args = flist[[f]])
+  })
+
+  keep <- genefilter::genefilter(SummarizedExperiment::assay(object, i = exprs_values), genefilter::filterfun(filter_functions))
+  return(object[keep, ])
+}
+
 #' EpOverA returns a filter function with bindings for A. This function evaluates to TRUE if the aggregate of the arguments elements are larger than A.
 #'
 #' @param A The value you want to exceed.
