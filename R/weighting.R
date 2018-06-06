8d3dce8096497204111df1b4cf8176a53f1b017f
Jens Preussner <jens.preussner@mpi-bn.mpg.de>
2018-06-06 14:04:35 +0200

    Added TF-IDF weighting.

--- R/weighting.R
+++ R/weighting.R
@@ -1,3 +1,34 @@
+#' Calculates the term frequency-inverse document frequency.
+#'
+#' @param object A SingleCellExperiment object.
+#' @param exprs_values String indicating which assay contains the data that should be used to calculate TF-IDF.
+#' @param features A character vector (of feature names), a logical vector or numeric vector (of indices) specifying the features to use for calculation The default of NULL will use all features.
+#' @param tf.method A character vector indicating the variant for calculating the TF.
+#' @param idf.weight A character vector indicating the weighting for IDF calculation.
+#'
+#' @return A \code{\link[Matrix]{sparseMatrix}}
+#'
+#' @export
+tf_idf <- function(object, exprs_values = "counts", features = NULL, tf.method = c("raw", "adjusted", "logscaled"), idf.weight = c("unary", "idf", "idf.smooth", "pidf")) {
+  input <- as.matrix(SummarizedExperiment::assay(object, i = exprs_values))
+  if (!is.null(features)) {
+    input <- as.matrix(SummarizedExperiment::assay(object, i = exprs_values)[features,])
+  }
+
+  tf <- switch(tf.method,
+               "raw" = input,
+               "adjusted" = t(t(input) / Matrix::colSums(input)),
+               "logscaled" = log(input + 1))
+
+  idf <- switch(idf.weight,
+                "unary" = rep(1, nrow(input)),
+                "idf" = -log(Matrix::rowSums(input) / ncol(input)),
+                "idf.smooth" = log(1 + ncol(input) / Matrix::rowSums(input)),
+                "pidf" = log((ncol(input) - Matrix::rowSums(input)) / Matrix::rowSums(input)))
+  idf <- methods::as(idf, "sparseVector")
+  methods::as(Matrix::Diagonal(x=as.vector(idf)), "sparseMatrix") %*% tf
+}
+
 #' Calculates weights/probabilities based on false-negative curves.
 #'
 #' @param data Expression data matrix
