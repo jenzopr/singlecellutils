110b786f81393fb2cb6174039dab5022bf885ab7
Jens Preussner <jens.preussner@mpi-bn.mpg.de>
2019-01-14 13:42:15 +0100

    Updated roxygen; Updated tf-idf

--- R/weighting.R
+++ R/weighting.R
@@ -5,11 +5,12 @@
 #' @param features A character vector (of feature names), a logical vector or numeric vector (of indices) specifying the features to use for calculation The default of NULL will use all features.
 #' @param tf.method A character vector indicating the variant for calculating the TF.
 #' @param idf.weight A character vector indicating the weighting for IDF calculation.
+#' @param slot Determines the name of the assay in \code{\link[SingleCellExperiment]{assays}} to use for weighted expression values.
 #'
-#' @return A \code{\link[Matrix]{sparseMatrix}}
+#' @return A \code{\link[SingleCellExperiment]{SingleCellExperiment}} object with modified \code{reducedDims} slot.
 #'
 #' @export
-tf_idf <- function(object, exprs_values = "counts", features = NULL, tf.method = c("raw", "adjusted", "logscaled"), idf.weight = c("unary", "idf", "idf.smooth", "pidf")) {
+tf_idf <- function(object, exprs_values = "counts", features = NULL, tf.method = c("raw", "binary", "adjusted", "logscaled"), idf.weight = c("unary", "idf", "idf.smooth", "pidf"), slot = "tdifd") {
   input <- as.matrix(SummarizedExperiment::assay(object, i = exprs_values))
   if (!is.null(features)) {
     input <- as.matrix(SummarizedExperiment::assay(object, i = exprs_values)[features,])
@@ -17,6 +18,7 @@ tf_idf <- function(object, exprs_values = "counts", features = NULL, tf.method =
 
   tf <- switch(tf.method,
                "raw" = input,
+               "binary" = as.matrix((input > 0) + 0),
                "adjusted" = t(t(input) / Matrix::colSums(input)),
                "logscaled" = log(input + 1))
 
@@ -26,5 +28,7 @@ tf_idf <- function(object, exprs_values = "counts", features = NULL, tf.method =
                 "idf.smooth" = log(1 + ncol(input) / Matrix::rowSums(input)),
                 "pidf" = log((ncol(input) - Matrix::rowSums(input)) / Matrix::rowSums(input)))
   idf <- methods::as(idf, "sparseVector")
-  methods::as(Matrix::Diagonal(x=as.vector(idf)), "sparseMatrix") %*% tf
+
+  SummarizedExperiment::assay(object, i = slot) <- methods::as(Matrix::Diagonal(x=as.vector(idf)), "sparseMatrix") %*% tf
+  return(object)
 }
