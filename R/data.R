17f4eabd84c5e65ae70531605b92bda279b0c7dd
Jens Preussner <jens.preussner@mpi-bn.mpg.de>
2019-02-05 14:06:49 +0100

    Added 10xgenomics 1K Brain cells dataset

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' 1k Brain Cells from an E18 Mouse (v3 chemistry)
+#'
+#' Single Cell Gene Expression Dataset by Cell Ranger 3.0.0
+#' Cells from a combined cortex, hippocampus and sub ventricular zone of an E18 mouse.
+#' * 1,301 cells detected
+#' * Sequenced on Illumina NovaSeq with approximately 71,000 reads per cell
+#' * 28bp read1 (16bp Chromium barcode and 12bp UMI), 91bp read2 (transcript), and 8bp I7 sample barcode
+#' * run with --expect-cells=1000
+#'
+#' @format A SingleCellExperiment object with 31053 features measured across 1301 samples:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{https://support.10xgenomics.com/single-cell-gene-expression/datasets/3.0.0/neuron_1k_v3}
+"neuron_1k_v3"
